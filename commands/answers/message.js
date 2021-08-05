4486a3998c8358c710064100dcddfebd135d23bc
Manas Khandelwal <mkdpsj@gmail.com>
2021-08-05 18:37:01 +0530

    message command improved

--- commands/answers/message.js
+++ commands/answers/message.js
@@ -1,6 +1,7 @@
 const Commando = require("discord.js-commando");
 const Discord = require("discord.js");
 const axios = require("axios");
+const ordinal = require("ordinal");
 const config = require("./../../config");
 
 module.exports = class AddCommand extends Commando.Command {
@@ -10,20 +11,42 @@ module.exports = class AddCommand extends Commando.Command {
       aliases: ["msg", "reason"],
       group: "answers",
       memberName: "message",
-      description: "Message/Reason for the latest HTML Question",
-      format: "message",
+      description: "Message/Reason for the HTML Question",
+      format: "message [user id|question no] [question no]",
+      argsType: "multiple",
       throttling: { usages: 1, duration: 14400 },
     });
   }
 
-  async run(message) {
-    const { id: userId } = message.author;
+  async run(message, args) {
+    const { id } = message.author;
+
+    let userId = id;
+    let questionNo = null;
+
+    if (args.length === 1) {
+      if (args[0].length === 18) userId = args[0];
+      else questionNo = args[0];
+    }
+
+    if (args.length === 2) {
+      if (args[0].length === 18) userId = args[0];
+      questionNo = args[1];
+    }
 
     const fetchedQuestion = await axios({
       method: "GET",
-      url: `${process.env.BASE_URL}htmlquestions/latest`,
+      url: `${process.env.BASE_URL}htmlquestions/${
+        questionNo === null ? "latest" : `?questionNo=${questionNo}`
+      }`,
     });
     const latestQuestion = fetchedQuestion.data.data.data[0];
+
+    if (!latestQuestion) {
+      return await message.reply(
+        `${ordinal(+questionNo)} Question does not exist!`
+      );
+    }
 
     const answerRequest = await axios({
       method: "PATCH",
@@ -32,15 +55,19 @@ module.exports = class AddCommand extends Commando.Command {
 
     if (!answerRequest.data.data) {
       return await message.channel.send(
-        `<@${userId}> has not answered the latest question yet.`
+        `<@${userId}> has not answered ${
+          questionNo === null ? "the latest" : ordinal(+questionNo)
+        } question.`
       );
     }
 
     const answer = answerRequest.data.data;
 
     const embed = new Discord.MessageEmbed()
-      .setTitle(`Points: ${answer.points}`)
-      .setDescription(`**Message:** *${answer.message}*`)
+      .setTitle(`HTML Quiz's ${ordinal(+questionNo)} Question`)
+      .setDescription(
+        `**User:** <@${userId}>\n**Points:** ${answer.points}\n**Message:** *${answer.message}*`
+      )
       .setColor(config.SUCCESS_COLOR);
 
     await message.channel.send(embed);
